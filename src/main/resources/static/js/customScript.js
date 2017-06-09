4cf86ec89fadb237acf91104baa306fb5edc0a74
EVBacic <evb6235@rit.edu>
2017-06-09 15:49:27 +0200

    Prototype functional with DB

--- src/main/resources/static/js/customScript.js
+++ src/main/resources/static/js/customScript.js
@@ -2,47 +2,45 @@
  * Created by emil-vid.bacic on 2.6.2017..
  */
 
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
+function generatePage(group_id){
+    clearRadar();
 
-var counter = 1;
-function populateCircle (r, c, items){
-    var angle = (2*Math.PI)/items.length;
-    for(var i = 1; i<=items.length; i++){
-        var xMove = c + r*(Math.cos(i*angle));
-        var yMove = c - r*(Math.sin(i*angle));
-        addItem(xMove, yMove, items[i-1]);
-        //counter++;
-    }
-}
-
-function generatePage(){
     var cat1 = [];
     var cat2 = [];
     var cat3 = [];
     var cat4 = [];
-    clearRadar();
     $.ajax({
         url: "/api/radar",
         type: "GET",
         contentType: 'application/json; charset=utf-8',
         success: function(resultData) {
+
+            var groupResultData = [];
+
             for(var i = 0; i < resultData.length; i++){
-                var temp = resultData[i];
+                var temp1 = resultData[i];
+                if(temp1.techGroup.id === group_id){
+                    groupResultData.push(temp1);
+                } else {
+
+                }
+            }
+
+            document.getElementById("radarTitle").innerHTML = "" + groupResultData[1].techGroup.name + " Tech Radar";
+            for(var x = 0; x < groupResultData.length; x++){
+                var temp = groupResultData[x];
                 switch(temp.category.id){
                     case 1:
-                        cat1.push(temp)
+                        cat1.push(temp);
                         break;
                     case 2:
-                        cat2.push(temp)
+                        cat2.push(temp);
                         break;
                     case 3:
-                        cat3.push(temp)
+                        cat3.push(temp);
                         break;
                     case 4:
-                        cat4.push(temp)
+                        cat4.push(temp);
                         break;
                 }
             }
@@ -66,81 +64,22 @@ function generatePage(){
     });
 
 }
-
-/**
-function populateOutCircle(items){
-    populateCircle(4.5, 4.8, items);
-}
-
-function populateMidCircle(items){
-    populateCircle(3.5, 4.8, items);
-}
-
-function populateInCircle(items){
-    populateCircle(2.5, 4.8, items);
-}
-
-function populateBullCircle(items){
-    if(items > 6){
-        populateCircle(1.5, 4.8, 6);
-        populateCircle(0.5, 4.8, items-6);
-    } else {
-        populateCircle(0.5, 4.8, items);
+function populateCircle (r, c, items){
+    var angle = (2*Math.PI)/items.length;
+    for(var i = 1; i<=items.length; i++){
+        var xMove = c + r*(Math.cos(i*angle));
+        var yMove = c - r*(Math.sin(i*angle));
+        addItem(xMove, yMove, items[i-1]);
     }
 }
-*/
-function clearRadar(){
-    svg = d3.select("svg");
-    svg.selectAll("g").remove();
-    counter = 1;
-    $("#list").empty();
-}
-
-// function populateRadar(id) {
-//     svg = d3.select("svg");
-//     clearRadar();
-//
-//     // ID ce se tu mijenjat i vuc podatke iz baze ovisno o tech koji user oce vidjet
-//     if(id == 1){
-//         var c3 = 7;
-//         var c2 = 6;
-//         var c1 = 5;
-//         var cBull = 7;
-//     }
-//
-//     if(id == 2){
-//         var c3 = 5;
-//         var c2 = 7;
-//         var c1 = 6;
-//         var cBull = 1;
-//     }
-//
-//     if(id == 3){
-//         var c3 = 2;
-//         var c2 = 9;
-//         var c1 = 1;
-//         var cBull = 10;
-//     }
-//
-//     if(id == 4){
-//         var c3 = 1;
-//         var c2 = 2;
-//         var c1 = 1;
-//         var cBull = 0;
-//     }
-//
-//     populateOutCircle(c3);
-//     populateMidCircle(c2);
-//     populateInCircle(c1);
-//     populateBullCircle(cBull);
-// }
-function addItem (x, y, item){
+function addItem (x, y, temp){
     var g = svg.append("g")
+        .attr("id", temp.id)
         .on("mouseover", function(){
-            handleHover(g, item.id);
+            handleHover(g, temp.id);
         })
         .on("mouseout", function(){
-            handleHoverOut(g, item.id);
+            handleHoverOut(g, temp.id);
         });
     var rect = g.append("rect")
         .attr("x", x)
@@ -152,16 +91,26 @@ function addItem (x, y, item){
         .attr("x", x + 0.1)
         .attr("y", y + 0.25)
         .attr("fill", "#fff")
-        .text(item.id)
+        .text(temp.id)
         .attr("overflow", "visible")
         .attr("font-size", 0.2);
-    $("#list").append("<li id='"+ item.id + "'>" + item.name + "</li>");
+    $("#list").append("<li id='" + temp.id + "'>" + " " + temp.id + ": " + temp.name + "</li>");
+
 }
 function handleHover (rect, num){
     rect.attr("opacity", 0.75);
-    $("#" + num).css("background-color", "#cecece");
+    $("#" + num).css("background-color", "green");
 }
 function handleHoverOut(rect, num){
     rect.attr("opacity", 1.0);
     $("#" + num).css("background-color", "white");
 }
+
+function clearRadar(){
+    svg = d3.select("svg");
+    svg.selectAll("g").remove();
+    counter = 1;
+    $("#list").empty();
+}
+
+
